e55f969f8ab58c16028fdbabb7b7cc1039c61617
Abdul Moez <abdulmoeztwitter@gmail.com>
2026-07-30 20:11:36 +0500

    feat: Add sign in and sign up functionality using clerk

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { FaCaretDown } from "react-icons/fa";
 import { IoCartOutline } from "react-icons/io5";
 import { Link, NavLink } from "react-router-dom";
+import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
 
 function Navbar() {
   const location = false;
@@ -27,16 +28,58 @@ function Navbar() {
 
         {/* Menu Section  */}
         <nav className="flex gap-7 items-center">
-            <ul className="flex gap-7 items-center font-semibold">
-                <NavLink to={'/'} className={({isActive}) => `${isActive ? "border-b-3 transition-all border-yellow-500" : "text-black"} cursor-pointer`}><li>Home</li></NavLink>
-                <NavLink to={'/products'} className={({isActive}) => `${isActive ? "border-b-3 transition-all border-yellow-500" : "text-black"} cursor-pointer`}><li>Products</li></NavLink>
-                <NavLink to={'/about'} className={({isActive}) => `${isActive ? "border-b-3 transition-all border-yellow-500" : "text-black"} cursor-pointer`}><li>About</li></NavLink>
-                <NavLink to={'/contact'} className={({isActive}) => `${isActive ? "border-b-3 transition-all border-yellow-500" : "text-black"} cursor-pointer`}><li>Contact</li></NavLink>
-            </ul>
-            <Link to={'/cart'} className="relative">
-            <IoCartOutline className="h-7 w-7"/>
-            <span className="bg-yellow-500 px-2 rounded-full absolute -top-2 -right-4">0</span>
-            </Link>
+          <ul className="flex gap-7 items-center font-semibold">
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+                `${isActive ? "border-b-3 transition-all border-yellow-500" : "text-black"} cursor-pointer`
+              }
+            >
+              <li>Home</li>
+            </NavLink>
+            <NavLink
+              to={"/products"}
+              className={({ isActive }) =>
+                `${isActive ? "border-b-3 transition-all border-yellow-500" : "text-black"} cursor-pointer`
+              }
+            >
+              <li>Products</li>
+            </NavLink>
+            <NavLink
+              to={"/about"}
+              className={({ isActive }) =>
+                `${isActive ? "border-b-3 transition-all border-yellow-500" : "text-black"} cursor-pointer`
+              }
+            >
+              <li>About</li>
+            </NavLink>
+            <NavLink
+              to={"/contact"}
+              className={({ isActive }) =>
+                `${isActive ? "border-b-3 transition-all border-yellow-500" : "text-black"} cursor-pointer`
+              }
+            >
+              <li>Contact</li>
+            </NavLink>
+          </ul>
+          <Link to={"/cart"} className="relative">
+            <IoCartOutline className="h-7 w-7" />
+            <span className="bg-yellow-500 px-2 rounded-full absolute -top-2 -right-4">
+              0
+            </span>
+          </Link>
+
+          <div>
+            <Show when="signed-out">
+              <div className="flex items-center gap-3">
+              <SignInButton className="bg-yellow-500 text-white font-bold px-3 py-1 rounded-md cursor-pointer " />
+              <SignUpButton className="bg-yellow-500 text-white font-bold px-3 py-1 rounded-md cursor-pointer" />
+              </div>
+            </Show>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
+          </div>
         </nav>
       </div>
     </div>
