469334b19b862c1b31b0830ccc9f743a2c5779c5
Abdul Moez <abdulmoeztwitter@gmail.com>
2026-07-31 20:42:57 +0500

    feat: Create a Function for getting location

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,9 @@ import React from "react";
 import { FaCaretDown } from "react-icons/fa";
 import { IoCartOutline } from "react-icons/io5";
 import { Link, NavLink } from "react-router-dom";
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
 
-function Navbar() {
-  const location = false;
+function Navbar({ location }) {
   return (
     <div className="bg-white py-3 shadow-2xl">
       <div className="max-w-6xl mx-auto flex justify-between items-center">
@@ -20,7 +19,18 @@ function Navbar() {
           <div className="flex gap-1 cursor-pointer text-gray-700 items-center">
             <MapPin className="text-yellow-600" />
             <span className="font-semibold">
-              {location ? <div></div> : "Add Address"}
+              {location ? (
+                <div className="-space-y-1 text-sm">
+                  <p>
+                    {location.city || location.state_district || location.state}
+                  </p>
+                  <p className="text-xs text-gray-500 font-normal">
+                    {location.country}
+                  </p>
+                </div>
+              ) : (
+                "Add Address"
+              )}
             </span>
             <FaCaretDown />
           </div>
@@ -72,8 +82,8 @@ function Navbar() {
           <div>
             <Show when="signed-out">
               <div className="flex items-center gap-3">
-              <SignInButton className="bg-yellow-500 text-white font-bold px-3 py-1 rounded-md cursor-pointer " />
-              <SignUpButton className="bg-yellow-500 text-white font-bold px-3 py-1 rounded-md cursor-pointer" />
+                <SignInButton className="bg-yellow-500 text-white font-bold px-3 py-1 rounded-md cursor-pointer " />
+                <SignUpButton className="bg-yellow-500 text-white font-bold px-3 py-1 rounded-md cursor-pointer" />
               </div>
             </Show>
             <Show when="signed-in">
