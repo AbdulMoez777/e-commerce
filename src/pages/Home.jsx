c68c64b06431da8ba448d7ee994e59800e881925
Abdul Moez <abdulmoeztwitter@gmail.com>
2026-08-07 18:28:15 +0500

    feat: Mid Banner Comp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Carousel from '../components/Carousel'
+import MidBanner from '../components/MidBanner'
+
 
 function Home() {
   return (
    <>
    <Carousel/>
-   </>
+   <MidBanner/>
+  </>
   )
 }
 
